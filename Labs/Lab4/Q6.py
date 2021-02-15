93ffbe8c265468619a56c2f7647f75115293ad27
abrahax01 <56842663+abrahax01@users.noreply.github.com>
2021-02-15 11:39:49 +0000

    Create Q6.py

---
+++ Labs/Lab4/Q6.py
@@ -0,0 +1,15 @@
+import random
+
+Word = "Hello"
+
+X = random.randint(0, len(Word) - 1)
+Y = random.randint(0, len(Word) - 1)
+
+while X >= Y:
+    X = random.randint(0, len(Word) - 1)
+    Y = random.randint(0, len(Word) - 1)
+
+new_Word = Word[:X] + Word[Y] + Word[X + 1: Y] + Word[X] + Word[Y + 1:]
+
+print("Result", new_Word)
+print("Characters swapped: ", Word[X], Word[Y])
